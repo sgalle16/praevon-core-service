d7c274cf3389ba0d02bc814cb1fbd762cc28b095
RottenReality <andresorregolopez@gmail.com>
2025-09-07 12:44:33 -0500

    chore: initial routes configuration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/user.js";
+import propertiesRouter from "./routes/properties.js";
+import rentalsRouter from "./routes/rentals.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +18,14 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+const coreServiceRouter = express.Router();
+
+coreServiceRouter.use('/auth', authRouter);
+coreServiceRouter.use('/users', userRouter);
+coreServiceRouter.use('/properties', propertiesRouter);
+coreServiceRouter.use('/rentals', rentalsRouter);
+
+app.use('/api/core-service/v1', coreServiceRouter);
 
 app.get('/health', (req, res) => res.json({
     ok:true
